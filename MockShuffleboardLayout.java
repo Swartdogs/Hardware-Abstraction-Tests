24de7ceec718607691fde80c2a20f1ac3c2bd997
Collin McIntyre <crm11396@gmail.com>
2021-07-16 17:40:18 -0500

    Add empty mock dashboard items

---
+++ MockShuffleboardLayout.java
@@ -0,0 +1,26 @@
+package frc.robot.abstraction;
+
+import java.util.Map;
+
+import edu.wpi.first.wpilibj.shuffleboard.BuiltInWidgets;
+
+public class MockShuffleboardLayout extends ShuffleboardLayout 
+{
+    @Override
+    protected NetworkTableBoolean addBoolean(String networkTableId, boolean defaultValue, BuiltInWidgets widget, Map<String, Object> properties) 
+    {
+        return null;
+    }
+
+    @Override
+    protected NetworkTableDouble addDouble(String networkTableId, double defaultValue, BuiltInWidgets widget, Map<String, Object> properties) 
+    {
+        return null;
+    }
+
+    @Override
+    protected NetworkTableString addString(String networkTableId, String defaultValue, BuiltInWidgets widget, Map<String, Object> properties) 
+    {
+        return null;
+    }    
+}
---
+++ MockShuffleboardTab.java
@@ -0,0 +1,33 @@
+package frc.robot.abstraction;
+
+import java.util.Map;
+
+import edu.wpi.first.wpilibj.shuffleboard.BuiltInLayouts;
+import edu.wpi.first.wpilibj.shuffleboard.BuiltInWidgets;
+
+public class MockShuffleboardTab extends ShuffleboardTab 
+{
+    @Override
+    protected NetworkTableBoolean addBoolean(String networkTableId, boolean defaultValue, int x, int y, int w, int h, BuiltInWidgets widget, Map<String, Object> properties) 
+    {
+        return null;
+    }
+
+    @Override
+    protected NetworkTableDouble addDouble(String networkTableId, double defaultValue, int x, int y, int w, int h, BuiltInWidgets widget, Map<String, Object> properties) 
+    {
+        return null;
+    }
+
+    @Override
+    protected NetworkTableString addString(String networkTableId, String defaultValue, int x, int y, int w, int h, BuiltInWidgets widget, Map<String, Object> properties) 
+    {
+        return null;
+    }
+
+    @Override
+    protected ShuffleboardLayout addLayout(String layoutName, BuiltInLayouts layout, int x, int y, int w, int h, Map<String, Object> properties) 
+    {
+        return null;
+    }
+}
